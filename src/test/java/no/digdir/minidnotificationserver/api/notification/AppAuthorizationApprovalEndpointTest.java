63097c69ead84a021bf738e68b6c7e9a0d487d21
Aase Mestad <aase.mestad@difi.no>
2021-03-15 13:56:50 +0100

    firebasebeans?

--- src/test/java/no/digdir/minidnotificationserver/api/notification/AppAuthorizationApprovalEndpointTest.java
+++ src/test/java/no/digdir/minidnotificationserver/api/notification/AppAuthorizationApprovalEndpointTest.java
@@ -1,47 +1,36 @@
 package no.digdir.minidnotificationserver.api.notification;
 
-import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.Message;
 import no.digdir.minidnotificationserver.api.internal.approval.RequestApprovalEntity;
+import no.digdir.minidnotificationserver.integration.firebase.FirebaseBeans;
 import no.digdir.minidnotificationserver.logging.audit.AuditService;
-import no.digdir.minidnotificationserver.service.AdminContext;
 import no.digdir.minidnotificationserver.service.RequestApprovalCache;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -68,6 +57,9 @@ public class AppAuthorizationApprovalEndpointTest {
 
     @Autowired
     private WebApplicationContext wac;
+
+    @Autowired
+    private FirebaseBeans firebaseBeans;
 
     @Test
     public void approvalTest() throws Exception {
