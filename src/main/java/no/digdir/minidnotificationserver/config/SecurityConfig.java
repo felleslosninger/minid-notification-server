515175ecd7c03c02d11e35b831b34bdaf90dd6b1
Andreas Balevik <andreas.balevik@digdir.no>
2021-03-22 13:24:28 +0000

    2021-03-22-1324-ba3641b|PBLEID-21524: Sjekk sikkerhetsheadere i SecurityConfig.java

--- src/main/java/no/digdir/minidnotificationserver/config/SecurityConfig.java
+++ src/main/java/no/digdir/minidnotificationserver/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.header.HeaderWriterFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
@@ -26,21 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .cors(withDefaults())
-                .csrf().disable()
-//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//            .and()
-//                .exceptionHandling()
-//                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)) // translate unauthenticated exception to a http status 401 response
-//            .and()
+//                .cors(withDefaults()) // TODO: check me
+                .csrf().disable() // TODO: check me
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // TODO: check me
                 .headers()
                 // check with online CSP evaluator at https://csp-evaluator.withgoogle.com/
-//                .contentSecurityPolicy("base-uri 'self'; object-src 'none'; default-src 'self'; connect-src 'self' localhost; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' ; font-src  'self' ; img-src 'self' data: ;  frame-ancestors 'none'; child-src 'self'; frame-src 'self'; require-trusted-types-for 'script")
-                //TODO: check csp and swagger
-//            .and()
+                .contentSecurityPolicy("base-uri 'self'; object-src 'none'; default-src 'self'; connect-src 'self'; script-src 'nonce-{{nonce}}' 'strict-dynamic' http: https: 'unsafe-inline'; style-src 'self' 'nonce-{{nonce}}'; font-src 'self'; img-src 'self' data:;  frame-ancestors 'none'; child-src 'self'; frame-src 'self';")
+            .and()
                 .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN)
             .and()
                 .and()
+                .addFilterBefore(new CSPNonceFilter(), HeaderWriterFilter.class)
                 .addFilterBefore(exceptionHandlerFilter, LogoutFilter.class)
                 .exceptionHandling()
                 .authenticationEntryPoint(problemSupport)
---
+++ src/main/java/no/digdir/minidnotificationserver/config/SwaggerNonceTranformer.java
@@ -0,0 +1,42 @@
+package no.digdir.minidnotificationserver.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springdoc.core.SwaggerUiConfigProperties;
+import org.springdoc.core.SwaggerUiOAuthProperties;
+import org.springdoc.webmvc.ui.SwaggerIndexPageTransformer;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.resource.ResourceTransformerChain;
+import org.springframework.web.servlet.resource.TransformedResource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class SwaggerNonceTranformer extends SwaggerIndexPageTransformer {
+
+    private static final String CSP_NONCE_ATTRIBUTE = "cspNonce";
+
+    public SwaggerNonceTranformer(SwaggerUiConfigProperties swaggerUiConfig, SwaggerUiOAuthProperties swaggerUiOAuthProperties, ObjectMapper objectMapper) {
+        super(swaggerUiConfig, swaggerUiOAuthProperties, objectMapper);
+    }
+
+    @Override
+    public Resource transform(HttpServletRequest request, Resource resource, ResourceTransformerChain transformerChain) throws IOException {
+        final AntPathMatcher antPathMatcher = new AntPathMatcher();
+        boolean isIndexFound = antPathMatcher.match("**/swagger-ui/**/index.html", resource.getURL().toString());
+
+        if (isIndexFound && hasDefaultTransformations()) {
+            String html = defaultTransformations(resource.getInputStream());
+            String nonce = request.getAttribute(CSP_NONCE_ATTRIBUTE).toString();
+            String htmlWithNonce = html
+                    .replace("<script", "<script nonce='" + nonce + "'")
+                    .replace("<style", "<style nonce='" + nonce + "'")
+                    ;
+            return new TransformedResource(resource, htmlWithNonce.getBytes());
+        }
+        else
+            return resource;
+    }
+}
